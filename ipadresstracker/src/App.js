ce4a6db1a4768f8fe7ebb2da09d3e15cc85db6b3
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-10-14 23:08:15 +0300

    all done

--- ipadresstracker/src/App.js
+++ ipadresstracker/src/App.js
@@ -1,7 +1,5 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
-
 import Map from "./components/Map";
 function App() {
   const [input, setInput] = useState("");
@@ -18,17 +16,16 @@ function App() {
       const res = await fetch("https://api.ipify.org/?format=json");
       const data = await res.json();
       setIP(data.ip);
-      throw new Error("error");
+      if (!res.ok) {
+        throw new Error("Cannot get IP address");
+      }
     } catch (error) {
       setError(error.message);
     }
     setLoading(false);
     // getting the ip address of the user
   };
-  useEffect(() => {
-    getData();
-  }, [setInput]);
-  const getPosition = async () => {
+  const getPosition = async (ip) => {
     try {
       const result = await fetch(`https://ipapi.co/${ip}/json/`);
       const data = await result.json();
@@ -38,7 +35,9 @@ function App() {
         latitude: data.latitude,
         longitude: data.longitude,
       });
-      throw new Error("error");
+      if (!result.ok) {
+        throw new Error("Cannot get location");
+      }
     } catch (error) {
       setError(error.message);
     }
@@ -46,89 +45,111 @@ function App() {
     // getting the location of the user using the ip address
   };
   useEffect(() => {
-    getPosition();
+    getData();
+    getPosition(ip);
   }, []);
 
-  const handleClick = () => {
-    setIP(input);
-    getPosition();
-    setError("");
-    setInput("");
+  const updateMapOnClick = async (ip) => {
+    try {
+      if (input) {
+        setIP(input);
+        setInput("");
+        const result = await fetch(`https://ipapi.co/${input}/json/`);
+        if (!result.ok) {
+          throw new Error("Cannot get your new location");
+        }
+        const data = await result.json();
+        setInfo(data);
+        // console.log(data);
+        setLocation({
+          latitude: data.latitude,
+          longitude: data.longitude,
+        });
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log("error from update", error);
+      setError(error.message);
+    }
   };
+
   return (
-    <div className="container-main absolute top-0 left-0 right-0 h-screen max-h-screen  ">
-      <div className="absolute left-0 right-0  mt-5   flex  justify-center ">
-        <h1 className="text-gray-100 font-bold mt-5 text-4xl ">
+    <div className="container-main flex flex-col relative  h-screen  gap-5">
+      <div className="search-bar-container h-2/5 min-w-full bg-cover  flex flex-col gap-5 justify-center items-center -mt-10 ">
+        <h2 className=" mb-3 text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl  items-center font-semibold  text-white">
           IP Address Tracker
-        </h1>
-      </div>
-      <div className="absolute left-0 right-0  mt-28   flex  justify-center gap-0">
-        <input
-          type="search"
-          className="rounded-s-xl text-xl p-2 h-14 w-1/4"
-          placeholder="Search for any IP address or domain"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={handleClick}>
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="search-icon w-12 bg-black h-14 p-2 rounded-e-xl"
-          >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {" "}
-              <path
-                d="M10 7L15 12L10 17"
-                stroke="#ffffff"
-                stroke-width="1.5"
+        </h2>
+        <div className=" md:w-2/5 w-4/5 sm:w-4/6  lg:w-1/3 items-center flex justify-center h-18 mt-0 mb-0">
+          <input
+            type="search"
+            className="w-full md:w-full xl:w-full sm:w-4/5 h-full rounded-s-lg border-input bg-background px-3 py-2 lg:py-6 lg:text-xl sm:text-sm ring-offset ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none placeholder:text-muted-foreground"
+            placeholder="Search for any IP address or domain"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={() => updateMapOnClick(ip)}>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="bg-black rounded-e-lg lg:h-20 w-14 lg:w-20 h-full flex justify-center items-center "
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-              ></path>{" "}
-            </g>
-          </svg>
-        </button>
-      </div>
-
-      <img
-        src="/images/pattern-bg-desktop.png"
-        className="pattern-bg w-full  h-80"
-        alt=""
-      />
-
-      <div className="absolute  bg-white text-gray-500 w-1/2 h-32 flex gap-4 justify-around p-4 rounded-xl left-0 right-0 mx-auto -mt-16 z-40  shadow-sm ">
-        <div className="  border-r-2 border-gray-400/50 ">
-          <h2 className="font-bold ">IP Address</h2>
-          <p className=" text-gray-900 mt-4 mr-4 text-xl">{ip}</p>
-        </div>
-        <div className="  border-r-2 border-gray-400/50 ">
-          <h2 className="font-bold ">Location</h2>
-          <p className=" text-gray-900 mt-4 mr-4 text-xl">
-            {info.country}, {info.city}
-          </p>
-        </div>
-        <div className="  border-r-2 border-gray-400/50 ">
-          <h2 className="font-bold ">Timezone</h2>
-          <p className=" text-gray-900 mt-4 mr-4 text-xl">
-            UTC {info.timezone}
-          </p>
-        </div>
-        <div>
-          <h2 className="font-bold mr-4">ISP</h2>
-          <p className=" text-gray-900 mt-4 mr-4 text-xl">{info.org}</p>
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  d="M10 7L15 12L10 17"
+                  stroke="#ffffff"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>{" "}
+              </g>
+            </svg>
+          </button>
         </div>
       </div>
-      <div className="h-96 absolute z-10 left-0 right-0  ">
-        {loading ? <p className="">Loading...</p> : null}
-        {error ? <p className="text-red-500 ">{error}</p> : null}
-        <Map center={location} />
+
+      <div className="w-4/5 stats  -mt-8 sm:-mt-10 md:mt-0 lg:-mt-8 rounded-lg  absolute  top-48 xl:top-64  md:top-38 left-14 lg:left-48 sm:left-0 bg-white z-50 flex flex-col sm:flex-row container gap-4 md:gap-10 p-4 md:p-8 ">
+        <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap-4 items-center ">
+          <p className="text-sm text-slate-400 font-semibold ">IP Address</p>
+          <p className="md:text-2xl text-black font-bold">
+            {loading ? "Loading..." : info.ip}
+          </p>
+        </div>
+        <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap-4 items-center ">
+          <p className="text-sm text-slate-400 font-semibold ">Location</p>
+          <p className="md:text-2xl text-black font-bold">
+            {loading ? "Loading..." : info.country}, {info.city}
+          </p>
+        </div>
+        <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap-4 items-center ">
+          <p className="text-sm text-slate-400 font-semibold ">Timezone</p>
+          <p className="md:text-2xl text-black font-bold">
+            UTC {loading ? "Loading..." : info.timezone}
+          </p>
+        </div>
+        <div className="w-full flex flex-col gap-2 items-center ">
+          <p className="text-sm text-slate-400 font-semibold mr-4">ISP</p>
+          <p className="md:text-2xl text-black font-bold">
+            {loading ? "Loading..." : info.org}
+          </p>
+        </div>
+      </div>
+
+      <div className="h-full w-full  top-96">
+        {loading ? (
+          <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+          </div>
+        ) : (
+          <Map center={location} error={error} />
+        )}
       </div>
     </div>
   );
--- ipadresstracker/src/components/Map.jsx
+++ ipadresstracker/src/components/Map.jsx
@@ -2,7 +2,6 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
 const Map = (props) => {
   const customIcon = new L.Icon({
     iconUrl: markerIcon,
@@ -12,21 +11,25 @@ const Map = (props) => {
     popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
     shadowSize: [41, 41], // size of the shadow
   });
-
   const position = [props.center.latitude, props.center.longitude];
-  // const position = [30.008, 31.2194];
-  console.log(position);
+
+  let state = {
+    keyMarker: Math.random(),
+  };
 
   return (
     <MapContainer
+      key={state.keyMarker}
       center={position}
       zoom={13}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
       style={{
-        height: "100vh",
-        width: "100vw",
-        position: "absolute!important",
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        zIndex: -1,
       }}
+      // className="w-full h-full"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
