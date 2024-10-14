b9bf307cd8009b37cb6b57f2277c89ae9b98d3c6
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-10-15 00:12:46 +0300

    fix some issues

--- ipadresstracker/src/App.js
+++ ipadresstracker/src/App.js
@@ -74,15 +74,15 @@ function App() {
   };
 
   return (
-    <div className="container-main flex flex-col relative  h-screen  gap-5">
-      <div className="search-bar-container h-2/5 min-w-full bg-cover  flex flex-col gap-5 justify-center items-center -mt-10 ">
-        <h2 className=" mb-3 text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl  items-center font-semibold  text-white">
+    <div className="container-main flex flex-col relative  h-screen  ">
+      <div className="search-bar-container h-2/4 min-w-full bg-cover  flex flex-col gap-5 justify-center items-center  ">
+        <h2 className="xl:text-4xl mb-3 text-2xl sm:text-2xl md:text-3xl lg:text-4xl  items-center font-semibold  text-white">
           IP Address Tracker
         </h2>
-        <div className=" md:w-2/5 w-4/5 sm:w-4/6  lg:w-1/3 items-center flex justify-center h-18 mt-0 mb-0">
+        <div className=" md:w-2/5 w-4/5 sm:w-4/6  lg:w-1/3 items-center flex justify-center h-18 mt-0 mb-0 ">
           <input
             type="search"
-            className="w-full md:w-full xl:w-full sm:w-4/5 h-full rounded-s-lg border-input bg-background px-3 py-2 lg:py-6 lg:text-xl sm:text-sm ring-offset ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none placeholder:text-muted-foreground"
+            className="md:w-3/4 w-3/4 lg:w-4/5 bg-white md:h-14 rounded-l-lg p-2 text-white focus:outline-none"
             placeholder="Search for any IP address or domain"
             value={input}
             onChange={(e) => setInput(e.target.value)}
@@ -92,7 +92,7 @@ function App() {
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="bg-black rounded-e-lg lg:h-20 w-14 lg:w-20 h-full flex justify-center items-center "
+              className="bg-black  rounded-e-lg h-10 sm:h-full md:max-h-14 w-14 lg:w-20   flex justify-center items-center "
             >
               <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
               <g
@@ -115,7 +115,7 @@ function App() {
         </div>
       </div>
 
-      <div className="w-4/5 stats  -mt-8 sm:-mt-10 md:mt-0 lg:-mt-8 rounded-lg  absolute  top-48 xl:top-64  md:top-38 left-14 lg:left-48 sm:left-0 bg-white z-50 flex flex-col sm:flex-row container gap-4 md:gap-10 p-4 md:p-8 ">
+      <div className="w-4/5 stats  lg:-mt-6 lg:left-32 rounded-lg  absolute  top-56  -md:ml-4  md:top-40 left-14  sm:left-0 bg-white z-50 flex flex-col sm:flex-row container gap-4 md:gap-10 p-4 md:p-8 xl:top-56 ">
         <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap-4 items-center ">
           <p className="text-sm text-slate-400 font-semibold ">IP Address</p>
           <p className="md:text-2xl text-black font-bold">
@@ -142,8 +142,7 @@ function App() {
         </div>
       </div>
 
-      <div className="h-full w-full  top-96">
-        {error && <p className="text-red-500">{error}</p>}
+      <div className="h-full w-full">
         {loading ? (
           <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
             <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
